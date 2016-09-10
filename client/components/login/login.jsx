96944d48b6ae659a8b0a4df1ce988b2dd4ee78b8
TonyMcKendry <TonyMcKendry@gmail.com>
2016-09-10 17:19:24 -0600

    Added a little more to the login page

--- client/components/login/login.jsx
+++ client/components/login/login.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Center, Page, VBox } from 'react-layout-components';
+import { Center, Page, VBox, Box } from 'react-layout-components';
 
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Divider from 'material-ui/Divider';
 
 
 export const Login = React.createClass({
@@ -12,13 +13,25 @@ export const Login = React.createClass({
       <Page>
         <Center>
           <VBox>
-            <TextField
-              floatingLabelText='Username'/>
-            <TextField
-              floatingLabelText='Password'/>
+            <VBox
+            style={{'width' : '250px'}}>
             <RaisedButton
+              label='Login with Google'
+              style={{'margin' : '10px'}}/>
+            <RaisedButton
+              label='Login with Facebook'
+              style={{'margin' : '10px'}}/>
+            </VBox>
+            <Divider style={{'backgroundColor' : "#e0e0e0", "width" : '75vw'}}/>
+            <VBox>
+              <TextField
+              floatingLabelText='Username'/>
+              <TextField
+              floatingLabelText='Password'/>
+              <RaisedButton
               label='Login'
               primary/>
+            </VBox>
           </VBox>
         </Center>
       </Page>
