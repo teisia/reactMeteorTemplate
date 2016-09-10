3afa7c8ee90893b0fba57cfdbc5e442d28ca2cc2
TonyMcKendry <TonyMcKendry@gmail.com>
2016-09-10 15:39:06 -0600

    committing some changes I made

---
+++ client/components/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Center, Page, VBox } from 'react-layout-components';
+import TextField from 'material-ui/TextField';
+
+
+export const Login = React.createClass({
+  displayName: 'Login',
+  render(){
+    <h1>This is the login page</h1>
+  }
+})
