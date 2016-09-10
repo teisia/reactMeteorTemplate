b6f0bd78694e02d697db5706ca5db6306b422a77
TonyMcKendry <TonyMcKendry@gmail.com>
2016-09-10 14:45:33 -0600

    getting ready to build login page

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Center, Page, Box } from 'react-layout-components';
 import { ReactMeteorData } from 'meteor/react-meteor-data';
 import {Meteor} from 'meteor/meteor';
 import {render} from 'react-dom'
@@ -9,13 +10,17 @@ export const Main = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData(){
     return{
-      
+
     }
   },
   render(){
     console.log('main is rendering')
     return (
-      <h1>This is the page</h1>
+      <Page>
+        <Box>
+          <h1>This is the page</h1>
+        </Box>
+      </Page>
     )
   }
 })
