ec07b36cde9b8c3940fc7f40ae81ada7cd526760
TonyMcKendry <TonyMcKendry@gmail.com>
2016-09-11 21:52:50 -0600

    navigation tabs are now selectable and styled

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
 import { Center, Page, VBox, Box } from 'react-layout-components';
 import Divider from 'material-ui/Divider';
+import PersonOutline from 'material-ui/svg-icons/social/person-outline'
+import People from 'material-ui/svg-icons/social/people'
+import DateRange from 'material-ui/svg-icons/action/date-range'
+import Equalizer from 'material-ui/svg-icons/av/equalizer'
+import FormatListNumbered from 'material-ui/svg-icons/editor/format-list-numbered'
+
 
 export const Footer = React.createClass({
   displayName: "Footer",
+  mixins: [ReactMeteorData],
+  getMeteorData(){
+    return{
+      selected: State.get(App.Constants.State.selectedTab)
+    }
+  },
   tabStyle: {
     box: {
       width: '20vw',
-      textAlign: 'center'
+      textAlign: 'center',
+      height : '70px',
+    },
+    selectedBox:{
+      width: '20vw',
+      textAlign: 'center',
+      height : '70px',
+      color: 'white',
+      backgroundColor: 'blue'
     },
     text: {
-      margin : '0 auto'
+      margin : '0 auto',
+      fontSize: '13px'
+    },
+    icon:{
+      margin: '0 auto',
+      marginTop: '15px'
+    },
+    selectedIcon:{
+      color: 'white',
+      margin: '0 auto',
+      marginTop: '15px'
     }
   },
   render(){
@@ -18,29 +48,43 @@ export const Footer = React.createClass({
         <VBox style={{
           'position' : 'absolute',
           'bottom' : '0',
-          'height' : '60px'
         }}
         justifyContent='space-between'>
           <Divider style={{
             'backgroundColor' : "#e0e0e0",
-            'width' : '100vw'
+            'width' : '100vw',
           }}/>
           <Box>
-            <Box style={this.tabStyle.box}>
-            <p style={this.tabStyle.text}>My Picks</p>
-            </Box>
-            <Box style={this.tabStyle.box}>
-            <p style={this.tabStyle.text}>League Picks</p>
-            </Box>
-            <Box style={this.tabStyle.box}>
-            <p style={this.tabStyle.text}>Standings</p>
-            </Box>
-            <Box style={this.tabStyle.box}>
-            <p style={this.tabStyle.text}>Weekly Scores</p>
-            </Box>
-            <Box style={this.tabStyle.box}>
-            <p style={this.tabStyle.text}>My Account</p>
-            </Box>
+            <VBox
+            style={this.data.selected == 1 ? this.tabStyle.selectedBox : this.tabStyle.box}
+            onClick={()=>{App.Utils.general.tabSelected(1)}}>
+              <People style={this.data.selected == 1 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>My Picks</span>
+            </VBox>
+            <VBox
+            style={this.data.selected == 2 ? this.tabStyle.selectedBox : this.tabStyle.box}
+            onClick={()=>{App.Utils.general.tabSelected(2)}}>
+              <FormatListNumbered style={this.data.selected == 2 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>League Picks</span>
+            </VBox>
+            <VBox
+            style={this.data.selected == 3 ? this.tabStyle.selectedBox : this.tabStyle.box}
+            onClick={()=>{App.Utils.general.tabSelected(3)}}>
+              <Equalizer style={this.data.selected == 3 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>Standings</span>
+            </VBox>
+            <VBox
+            style={this.data.selected == 4 ? this.tabStyle.selectedBox : this.tabStyle.box}
+            onClick={()=>{App.Utils.general.tabSelected(4)}}>
+              <DateRange style={this.data.selected == 4 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>This Week</span>
+            </VBox>
+            <VBox
+            style={this.data.selected == 5 ? this.tabStyle.selectedBox : this.tabStyle.box}
+            onClick={()=>{App.Utils.general.tabSelected(5)}}>
+              <PersonOutline style={this.data.selected == 5 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>Profile</span>
+            </VBox>
           </Box>
         </VBox>
     )
--- client/constants/dispatch.js
+++ client/constants/dispatch.js
@@ -1,6 +1,9 @@
 var dispatches = {
   login: {
     success: "SUCCESSFULLY_LOGGED_IN"
+  },
+  tab: {
+    selected: "A_TAB_HAS_BEEN_SELECTED"
   }
 }
 
